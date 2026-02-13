e333ccfd476989dbdf0c81ec3f31b625ad259537
Rolando Rodriguez <rolandognr@gmail.com>
2026-02-13 15:16:47 -0600

    Fixed image

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -4,7 +4,11 @@ import { Link } from "react-router-dom"
 export default function ProductCard({product}) {
     return (
         <div className="product-card">
-                        <img src={product.image} className="product-card-image"/>
+                        <img
+                            src={product.image} 
+                            alt={product.name}
+                            className="product-card-image"
+                        />
                         <div className="product-card-content">
                             <h3>{product.name}</h3>
                             <p>{product.price}</p>
