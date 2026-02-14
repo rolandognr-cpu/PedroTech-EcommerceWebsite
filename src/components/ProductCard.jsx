c5fa633686d0761275e4acc678844917ad419c52
Rolando Rodriguez <rolandognr@gmail.com>
2026-02-14 12:42:50 -0600

    complete

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,22 +1,27 @@
 import { Link } from "react-router-dom"
+import { useCart } from "../context/CartContext"
 
 
 export default function ProductCard({product}) {
+    const { cartItems, addToCart } = useCart()
+    const productInCart = cartItems.find((item) => item.id === product.id)
+    const productQuantityLabel = productInCart ? `(${productInCart.quantity})` : ""
+
     return (
         <div className="product-card">
-                        <img
-                            src={product.image} 
-                            alt={product.name}
-                            className="product-card-image"
-                        />
-                        <div className="product-card-content">
-                            <h3>{product.name}</h3>
-                            <p>{product.price}</p>
-                            <div className="product-card-actions">
-                                <Link className="btn btn-secondary" to={`/products/${product.id}`}>View Details</Link>
-                                <button className="btn btn-prymary">Add to Cart</button>
-                            </div>
-                        </div>
-                    </div>
+            <img
+                src={product.image} 
+                alt={product.name}
+                className="product-card-image"
+            />
+            <div className="product-card-content">
+                <h3>{product.name}</h3>
+                <p>{product.price}</p>
+                <div className="product-card-actions">
+                    <Link className="btn btn-secondary" to={`/products/${product.id}`}>View Details</Link>
+                    <button className="btn btn-prymary" onClick={() => addToCart(product.id)}>Add to Cart {productQuantityLabel}</button>
+                </div>
+            </div>
+        </div>
     )
 }
--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,11 +1,14 @@
 import { useEffect, useState } from "react"
 import { useNavigate, useParams } from "react-router-dom"
 import { getProductById } from "../data/products"
+import { useCart } from "../context/CartContext"
 
 export default function ProductDetails() {
     const {id} = useParams()
     const [product, setProduct] = useState(null)
     const navigate = useNavigate()
+    const { cartItems, addToCart } = useCart()
+    
 
     useEffect(()=> {
 
@@ -25,6 +28,9 @@ export default function ProductDetails() {
         return <h1>Loading...</h1>
     }
 
+    const productInCart = cartItems.find((item) => item.id === product.id)
+    const productQuantityLabel = productInCart ? `(${productInCart.quantity})` : ""
+
     return (
         <div className="page">
             <div className="container">
@@ -36,7 +42,7 @@ export default function ProductDetails() {
                         <h1 className="product-detail-name">{product.name}</h1>
                         <p className="product-detail-price">{product.price}</p>
                         <p className="product-detail-description">{product.descriotion}</p>
-                        <button className="btn btn-primary">Add to Cart</button>
+                        <button className="btn btn-primary" onClick={() => addToCart(product.id)}>Add to Cart {productQuantityLabel}</button>
                     </div>
                 </div>
             </div>
