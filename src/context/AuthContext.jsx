60e6abf2626d62f454f30a9a05d063c52bf277d5
Rolando Rodriguez <rolandognr@gmail.com>
2026-02-13 20:30:38 -0600

    added auth

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,45 @@
+import { createContext, useState } from "react";
+
+export const AuthContext = createContext(null);
+
+export default function AuthProvider({children}) {
+    const [user, setUser] = useState(
+        localStorage.getItem('currentUserEmail')
+        ? { email: localStorage.getItem('currentUserEmail') }
+        : null
+    )
+
+    function signUp(email, password) {
+        const users = JSON.parse(localStorage.getItem('users') || "[]")
+
+        if (users.find((u) => u.email === email)) {
+            return { success: false, error: "Email already exists" }
+        }
+
+
+        const newUser = {email, password}
+        users.push(newUser)
+        localStorage.setItem("users", JSON.stringify(users))
+        return { success: true }
+    }
+
+    function logIn(email, password) {
+        const users = JSON.parse(localStorage.getItem('users') || "[]")
+        const user = users.find((u) => u.email === email && u.password === password)
+
+        if(!user) {
+            return { success: false, error: "Invalid email or password" }
+        }
+
+        localStorage.setItem("currentUserEmail", email)
+        setUser({ email })
+
+        return { success: true }
+    }
+
+    function logOut() {
+        localStorage.removeItem('currentUserEmail')
+        setUser(null)
+    }
+    return <AuthContext.Provider value={ { logIn, signUp, user, logOut} }>{children}</AuthContext.Provider>
+}
--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,3 +1,111 @@
+import { useContext, useState } from "react"
+import { useForm } from "react-hook-form"
+import { AuthContext } from "../context/AuthContext"
+import { useNavigate } from "react-router-dom"
+
+
 export default function Auth() {
-    return <div>Auth Pge</div>
-}
+    const[mode, setMode] = useState('signup')
+    const[error, setError] = useState(null)
+    const { logIn, signUp, user, logOut } = useContext(AuthContext)
+    const navigate = useNavigate()
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors }
+    } = useForm()
+
+    function onSubmit(data) {
+        setError(null)
+        let result
+        if (mode === "signup") {
+            result = signUp(data.email, data.password)
+        } else {
+            result = logIn(data.email, data.password)
+        }
+
+        if (result.success) {
+            if (mode === "signup") {
+                alert("Account created! Go ahead and log in!")
+                setMode("signin")
+            } else {
+                navigate("/")
+            }
+        } else {
+            setError(result.error)
+        }
+
+    }
+
+
+    return (
+    <div className="page">
+        <div className="container">
+            <div className="auth-container">
+                { user && (<p>User logged in: {user.email} </p>)}
+                <button onClick={() => logOut()}>Log out</button>
+                <h1 className="page-title">
+                    {mode === 'signup' ? 'Sign Up' : "Login"}
+                </h1>
+                <form className="auth-form" onSubmit={handleSubmit(onSubmit)}>
+                    { error && <div className="error-message">{ error }</div>}
+                    <div className="form-group">
+                        <label className="form-label" htmlFor="email">
+                            Email
+                        </label>
+                        <input
+                            className="form-input"
+                            type="email"
+                            id="email"
+                            {...register('email', { required: "Email is required"})}
+                        />
+                        {errors.email && 
+                            <span className="form-error">{errors.email.message}</span>}
+                    </div>
+
+                    <div className="form-group">
+                        <label className="form-label" htmlFor="password">
+                            Password
+                        </label>
+                        <input
+                            {...register('password', {
+                                required: "Password is required",
+                                minLength: {
+                                    value: 6,
+                                    message:"Password must be at least 6 characters"
+                                },
+                                maxLength: {
+                                    value: 12,
+                                    message: "Password must be 12 cgaracters or less"
+                                }
+                            })}
+                            className="form-input" type="password" id="password"
+                        />
+                        {errors.password && <span className="form-error">
+                            {errors.password.message}
+                            </span>}
+                    </div>
+
+                    <button type="submit" className="btn btn-primary btn-large">
+                        {mode === 'signup' ? 'Sign Up' : "Login"}
+                    </button>
+                </form>
+
+                <div className="auth-switch">
+                    {mode === 'signup' ? (
+                        <p>
+                            Already have an account?{" "}
+                            <span className="auth-link" onClick={() => (setMode("login"))}>Login</span>
+                        </p>
+                    ) : (
+                        <p>
+                            Don't have an account?{' '}
+                            <span className="auth-link" onClick={() => (setMode("signup"))}>Sign Up</span>
+                        </p>
+                    )}
+                </div>
+            </div>
+        </div>
+    </div>
+)}
